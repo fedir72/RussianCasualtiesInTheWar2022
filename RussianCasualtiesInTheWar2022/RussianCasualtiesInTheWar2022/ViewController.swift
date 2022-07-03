3724876495d8c13ae6c64366d0dc26265d77583f
fedir72 <fedir72@ua.fm>
2022-07-03 11:36:21 +0300

    + start project

---
+++ RussianCasualtiesInTheWar2022/RussianCasualtiesInTheWar2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RussianCasualtiesInTheWar2022
+//
+//  Created by Fedii Ihor on 03.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
