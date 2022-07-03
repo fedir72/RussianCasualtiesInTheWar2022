55220403e427f902adabeb62a6100a09ca407d69
fedir72 <fedir72@ua.fm>
2022-07-03 12:07:46 +0300

    + primary project stucture

---
+++ RussianCasualtiesInTheWar2022/RussianCasualtiesInTheWar2022/Views/CategoryController/CategoryViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CategoryViewController.swift
+//  RussianCasualtiesInTheWar2022
+//
+//  Created by Fedii Ihor on 03.07.2022.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+    // MARK: - Types
+
+    // MARK: - Constants
+
+    // MARK: - Public Properties
+
+    // MARK: - Subview Properties
+
+    // MARK: - Private Properties
+
+    // MARK: - UIViewController(*)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Public Methods
+
+    // MARK: - Private Methods
+  
+
+}
+
---
+++ RussianCasualtiesInTheWar2022/RussianCasualtiesInTheWar2022/Views/DetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  DetailViewController.swift
+//  RussianCasualtiesInTheWar2022
+//
+//  Created by Fedii Ihor on 03.07.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    // MARK: - Types
+
+    // MARK: - Constants
+
+    // MARK: - Public Properties
+
+    // MARK: - Subview Properties
+
+    // MARK: - Private Properties
+
+    // MARK: - UIViewController(*)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Public Methods
+
+    // MARK: - Private Methods
+  
+}
